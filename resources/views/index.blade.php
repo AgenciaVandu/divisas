b15251eb3344d73b3edcc5b4ff92c93dbb5ceeab
alvarbu <74802594+alvarbu@users.noreply.github.com>
2022-07-05 11:11:03 -0500

    se elimina frase de dolar especial

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -141,7 +141,7 @@
                                     <h2 class="titular-compra"> Venta <span>${{  number_format($dolar_especial->venta,2) }}</span></h2>
                                 </div>
                             </div>
-                            <small class="text-center">Tipo de Cambio informativo, sujeto a variación sin previo aviso. Nos Reservamos el Derecho de Compra y/o Venta de Cualquier Divisa. Aplica sólo en billetes de 1, 5, 10, 20. Sujeto a disponibilidad únicamente en sucursales: Plaza Fiesta y Colonia México.</small>
+                            <small class="text-center">Aplica sólo en billetes de 1, 5, 10, 20. Sujeto a disponibilidad únicamente en sucursales: Plaza Fiesta y Colonia México.</small>
                         </div>
                     </div>
                 </div>
